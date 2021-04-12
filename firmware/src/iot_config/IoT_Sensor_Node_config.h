d1ef2bbe7d024bc597650d23059ea2a8baaa1091
Daisuke Nakahara <daisuken@msn.com>
2021-04-12 13:09:38 -0700

    Enable Debug Logging

--- firmware/src/iot_config/IoT_Sensor_Node_config.h
+++ firmware/src/iot_config/IoT_Sensor_Node_config.h
@@ -5,7 +5,7 @@
 
 #define CFG_TIMEOUT 5000
 
-#define CFG_DEBUG_MSG  0 //set to enable debug print messages for cloud and wifi services
+#define CFG_DEBUG_MSG  1 //set to enable debug print messages for cloud and wifi services
 
 #define CFG_ENABLE_CLI 1
 
